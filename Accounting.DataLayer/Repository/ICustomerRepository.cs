34b188a71a4ea012d1b5ae38b7362cbbbdb6a8c6
Ali <aliaboli6749@gmail.com>
2022-08-16 11:10:52 +0430

    Start project

---
+++ Accounting.DataLayer/Repository/ICustomerRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting.DataLayer.Repository
+{
+    public interface ICustomerRepository
+    {
+        List<Customers> GetAllCustomers();
+        Customers GetCustomerbyId(int customerId);
+        bool InsertCustomer(Customers customer);
+        bool UpdateCustomer(Customers customer);
+        bool DeleteCustomer(Customers customer);
+        bool DeleteCustomer(int customerId);
+        void Svae();
+    }
+}
---
+++ Accounting.DataLayer/Services/CustomerRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Accounting.DataLayer.Repository;
+
+namespace Accounting.DataLayer.Services
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        AccountingEntities db = new AccountingEntities();
+        public bool DeleteCustomer(Customers customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteCustomer(int customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customers> GetAllCustomers()
+        {
+            return db.Customers.ToList();
+        }
+
+        public Customers GetCustomerbyId(int customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool InsertCustomer(Customers customer)
+        {
+            try
+            {
+                db.Customers.Add(customer);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public void Svae()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateCustomer(Customers customer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
